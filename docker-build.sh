db490beabdca27c88ff705b4eb44da4c30df7a6c
Erik Tesar <erik@erik-tesar.com>
2024-10-01 06:09:58 +0200

    add new survival server file and add a few plugins

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
 docker build . --file Dockerfile.base --tag ghcr.io/erik1000/minecraft-server-docker:base-latest --no-cache
 docker build . --file Dockerfile.velocity --tag ghcr.io/erik1000/minecraft-server-docker:velocity-latest --no-cache
 docker build . --file Dockerfile.plot --tag ghcr.io/erik1000/minecraft-server-docker:plot-latest --no-cache
+docker build . --file Dockerfile.grastaxi --tag ghcr.io/erik1000/minecraft-server-docker:grastaxi-latest --no-cache
